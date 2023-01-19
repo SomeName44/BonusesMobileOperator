18fac2336441125d7188876d75bf54a30afedbef
Olga <deputat.d@yandex.com>
2023-01-19 16:30:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        //Колличество денег на счету клиента
+        int amount = 100;
+        //сумма пополнения
+        int replenishment = 1000;
+        int balance;
+        int bonus;
+
+        if (replenishment < 1000) {
+            bonus = replenishment +100;
+
+        } else {
+            bonus = replenishment / 100;
+        }
+            balance = amount + replenishment + bonus;
+            System.out.println("Колличество" + " " + bonus + " " + "бонусных рублей" );
+            System.out.println("Итоговый счет" + " " + balance + " " + "рублей" );
+        }
+
+    }
+
+
+
